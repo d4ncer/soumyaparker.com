138a2805511073c5e2097d745bae992137cfe934
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-07-21 16:00:58 +1200

    Set up colors

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -4,9 +4,8 @@ export default {
     sans: 'Raleway, sans-serif',
   },
   color: {
-    primary: '',
-    secondary: '',
-    black: '',
-    white: '',
+    primary: '#F16C68',
+    secondary: '#5C5D60',
+    test: '#90847D',
   },
 };
