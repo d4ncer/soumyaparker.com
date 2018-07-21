254bac713a8ce332748402341f0ded1467d58921
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-07-21 17:35:22 +1200

    Add dummy project & about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const AboutPage = () => (
+  <Layout>
+    <div>Hi!</div>
+  </Layout>
+);
+
+export default AboutPage;
---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const ProjectsPage = () => (
+  <Layout>
+    <div>Hi!</div>
+  </Layout>
+);
+
+export default ProjectsPage;
