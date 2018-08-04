52bd2dbc2f2de4270e945f6f69711bad59e33c41
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-08-04 16:07:15 +1200

    Add coming soon placeholders for incomplete pages

---
+++ src/components/shared.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ComingSoon = styled.div`
+  height: 75vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import { ComingSoon } from '../components/shared';
+import { H2 } from '../utils/type';
 
 const AboutPage = () => (
   <Layout>
-    <div>Hi!</div>
+    <ComingSoon>
+      <H2>Coming soon</H2>
+    </ComingSoon>
   </Layout>
 );
 
