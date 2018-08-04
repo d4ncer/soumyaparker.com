e66aa5b04db1bbb5b171f64742c8424636b5392f
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-08-04 15:22:26 +1200

    Fix images

--- src/components/bg-image.js
+++ src/components/bg-image.js
@@ -1,27 +1,11 @@
+import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
 
 const BgImage = styled(Img)`
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: -1;
-  height: ${({ height }) => height || 'auto'};
-
-  & > img {
-    object-fit: ${({ fit }) => fit || 'cover'} !important;
-    object-position: ${({ position }) => position || '50% 50%'} !important;
-    font-family: 'object-fit: ${({ fit }) =>
-      fit || 'cover'} !important; object-position: ${({ position }) =>
-  position || '50% 50%'} !important;'
-  }
-`;
-
-export const BgImageRaw = styled.img`
-  position: absolute;
-  top: 0;
-  left: 0;
+  top: ${({ top }) => top || 0};
+  left: ${({ left }) => left || 0};
   width: 100%;
   z-index: -1;
   height: ${({ height }) => height || 'auto'};
