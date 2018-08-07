ad6735949abcf89e7225b5570f90facaeacf7b77
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-08-07 17:24:03 +1200

    Add temporary projects page with link to Behance

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
 import Layout from '../components/layout';
 import { ComingSoon } from '../components/shared';
-import { H2 } from '../utils/type';
+import { H2, P } from '../utils/type';
+
+import styles from '../utils/styles';
+
+const A = styled(OutboundLink)`
+  color: ${styles.color.grey};
+  cursor: pointer;
+
+  &:hover {
+    color: ${styles.color.interact};
+  }
+`;
 
 const ProjectsPage = () => (
   <Layout>
     <ComingSoon>
-      <H2>Coming soon</H2>
+      <H2>Under construction</H2>
+      <P>
+        You can see my work on{' '}
+        <A href="https://www.behance.net/soumyaparker">Behance</A> in the
+        meantime.
+      </P>
     </ComingSoon>
   </Layout>
 );
