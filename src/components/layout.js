febbc994d6d4ce2d978ec6fa9c60a6c203fd8a61
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-04-25 08:01:03 +1200

    Update import
    `react-helmet` no longer default exports `Helmet`.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
